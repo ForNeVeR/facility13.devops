1acc907ea816a2f65cee614271cf5f1206bc27c5
Friedrich von Never <friedrich@fornever.me>
2025-02-06 00:56:53 +0100

    ansible-playbook script: enable SSH pipelining

--- ansible-playbook.ps1
+++ ansible-playbook.ps1
@@ -1,1 +1,1 @@
-wsl --distribution Ubuntu-24.04 ansible-playbook --inventory hosts.ini @args
+wsl --distribution Ubuntu-24.04 ansible-playbook --inventory hosts.ini @args -e 'ansible_ssh_pipelining=True'
